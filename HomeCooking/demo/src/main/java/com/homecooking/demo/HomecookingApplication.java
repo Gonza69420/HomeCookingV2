48f0be8eb7e296ac3ba6f64cac07604d14c05dac
Gonza69420 <gonzalo.mato@ing.austral.edu.ar>
2022-03-25 14:57:48 -0300

    first commit

---
+++ HomeCooking/demo/src/main/java/com/homecooking/demo/HomecookingApplication.java
@@ -0,0 +1,13 @@
+package com.homecooking.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomecookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomecookingApplication.class, args);
+	}
+
+}
