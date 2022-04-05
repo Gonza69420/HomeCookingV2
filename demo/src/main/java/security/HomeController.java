8da60d274f130e318827b55e12f67b7f710772df
NachoEstevo <ignacio.estevo@ing.austral.edu.ar>
2022-04-04 23:41:48 -0300

    Added OAuth with Google Login

---
+++ demo/src/main/java/security/HomeController.java
@@ -0,0 +1,27 @@
+package security;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.security.Principal;
+
+public class HomeController {
+
+    @RequestMapping("/home")
+    public String home() {
+        return "home.jsp";
+    }
+    @RequestMapping("/login")
+    public String loginPage() {
+        return "login.jsp";
+    }
+    @RequestMapping("/logout-success")
+    public String logoutPage() {
+        return "logout.jsp";
+    }
+    @RequestMapping("/user")
+    @ResponseBody
+    public Principal user(Principal principal) {
+        return principal;
+    }
+}
--- demo/src/main/java/security/SecurityConfiguration.java
+++ demo/src/main/java/security/SecurityConfiguration.java
@@ -1,4 +1,21 @@
 package security;
 
-public class SecurityConfiguration {
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .antMatchers("/", "/home").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .formLogin()
+            .loginPage("/login")
+            .permitAll()
+            .and()
+            .logout()
+            .permitAll();
+    }
 }
